99908fd059d5973a6f4b74c824e0d8c184a8625c
Анастасия <89359100+Asenka90@users.noreply.github.com>
2022-12-25 10:50:27 +0400

    Add files via upload

---
+++ bot.py
@@ -0,0 +1,5 @@
+from aiogram import Bot
+from aiogram.dispatcher import Dispatcher
+
+bot = Bot(token='5677453204:AAG_woquRF2cHDock0FL2Q8LLh8GmKATAlY')
+dp = Dispatcher(bot)
---
+++ model.py
@@ -0,0 +1,4 @@
+count = 150
+turn = 29
+draw = 1 
+winner = 1
